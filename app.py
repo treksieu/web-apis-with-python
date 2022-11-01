72787090d4f650ba0aa111fabeee2b9ea5be3144
peti <gaspar@bardi.ro>
2022-11-02 00:55:59 +0200

    final

--- app.py
+++ app.py
@@ -7,17 +7,12 @@ app = Flask(__name__)
 
 @app.get("/")
 def index():
-    """
-    DEFAULT ROUTE
-    This method will
-    1. Provide usage instructions formatted as JSON
-    """
-    # response = {"usage": "/dict?=<word>"}
-    # Since this is a website with front-end, we don't need to send the usage instructions
-    return "TODO"
+  response = {"usage":"/dict?=<word>"}
+  return jsonify(response)
 
 
 @app.get("/dict")
+
 def dictionary():
     """
     DEFAULT ROUTE
@@ -26,8 +21,22 @@ def dictionary():
     2. Try to find an exact match, and return it if found
     3. If not found, find all approximate matches and return
     """
-    return "TODO"
+    word = request.args.get("word")
+    if not word:
+        return jsonify({"status":"error","data":"pls choose a word"})
 
+    # Try to find an exact match
 
+    definitions = match_exact(word)
+    if definitions:
+        return jsonify({"status":"success", "data":definitions})
+
+    # Try to find an approximate match
+    definitions = match_like(word)
+    if definitions:
+        return jsonify({"status":"partial","data":definitions})
+    else:
+        return jsonify({"status":"error","data":"word not found"})
+    
 if __name__ == "__main__":
     app.run()
