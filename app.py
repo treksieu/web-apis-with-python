d8e5b7aa9bf1f7524514bc3b2559e28f17d17deb
peti <gaspar@bardi.ro>
2022-11-01 21:33:52 +0200

    test

--- app.py
+++ app.py
@@ -1,22 +1,17 @@
+from ast import arg
 from flask import Flask, request, render_template, redirect
 
 app = Flask(__name__)
 
 @app.get("/")
 def index():
-    """
-    TODO: Render the home page provided under templates/index.html in the repository
-    """
-    return "TODO"
+   
+    return render_template("index.html")
 
 @app.get("/search")
 def search():
-	"""
-	TODO:
-	1. Capture the word that is being searched
-	2. Seach for the word on Google and display results
-	"""
-    return "TODO"
+    args = request.args.get("q")
+    return redirect(f"https://google.com/search?q={args}")
 
 if __name__ == "__main__":
     app.run()
