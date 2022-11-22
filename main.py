5db64c3f1baf38944649189d6131575d3d04c62f
peti <gaspar@bardi.ro>
2022-11-22 22:45:02 +0200

    fast

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-from typing import Optional
-from fastapi import FastAPI
-from model.dbHandler import match_exact, match_like
+from typing import List,Optional 
+from fastapi import FastAPI,Query
+from fastapi.encoders import jsonable_encoder
+from model.dbHandler import match_exact,match_like
 
-app = Flask(__name__)
+app = FastAPI()
 
 
 @app.get("/")
@@ -12,16 +13,32 @@ def index():
     This method will
     1. Provide usage instructions formatted as JSON
     """
-    return "TODO"
+    response = {"usage":"/dict?=<word>"}
+    return jsonable_encoder(response)
 
 
 @app.get("/dict")
-def dictionary():
-    """
-    DEFAULT ROUTE
-    This method will
-    1. Accept a word from the request
-    2. Try to find an exact match, and return it if found
-    3. If not found, find all approximate matches and return
-    """
-    return "TODO"
+
+def dictionary(word : str):
+
+    if not word:
+        response = {"status":"error","data":"pls choose a word"}
+        return jsonable_encoder(response)
+
+    # Try to find an exact match
+
+    definitions = match_exact(word)
+    if definitions:
+        response = {"status":"success","word":word, "data":definitions}
+        return jsonable_encoder(response)
+
+    # Try to find an approximate match
+
+    definitions = match_like(word)
+    if definitions:
+        response = {"status":"partial","word":word,"data":definitions}    
+        return jsonable_encoder(response)
+
+    else:
+        response = {"status":"error","word": word,"data":"word not found"}
+        return jsonable_encoder(response)
