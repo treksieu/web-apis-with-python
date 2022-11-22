0bdf1396aec91e293eab3cecee0763596d64fff0
peti <gaspar@bardi.ro>
2022-11-22 21:21:35 +0200

    all

--- importrequests.py
+++ importrequests.py
@@ -5,8 +5,9 @@ import io
 file = {"image": open("sample.jpg","rb")}
 headers = {"type":"multipart/image"}
 
-URL ="http://127.0.0.1:5000"
-filter ="emboss"
+URL ="https://treky.herokuapp.com/"
+
+filter ="blur"
 
 response = requests.post(f"{URL}/{filter}",headers=headers, files=file)
 response.raise_for_status
