f9abc74f6f6fa141f259674376e6bebb76853f65
peti <gaspar@bardi.ro>
2022-11-23 20:10:38 +0200

    test

--- main.py
+++ main.py
@@ -1,9 +1,32 @@
 from typing import List,Optional 
-from fastapi import FastAPI,Query
+from fastapi import FastAPI, Query
 from fastapi.encoders import jsonable_encoder
 from model.dbHandler import match_exact,match_like
+from fastapi.openapi.utils import get_openapi
+
 
 app = FastAPI()
+
+
+
+
+def custom_openapi():
+    if app.openapi_schema:
+        return app.openapi_schema
+    openapi_schema = get_openapi(
+        title="peti schema",
+        version="2.5.0",
+        description="This is a very custom OpenAPI schema",
+        routes=app.routes,
+    )
+    openapi_schema["info"]["x-logo"] = {
+        "url": "https://fastapi.tiangolo.com/img/logo-margin/logo-teal.png"
+    }
+    app.openapi_schema = openapi_schema
+    return app.openapi_schema
+
+
+app.openapi = custom_openapi
 
 
 @app.get("/")
@@ -42,3 +65,9 @@ def dictionary(word : str):
     else:
         response = {"status":"error","word": word,"data":"word not found"}
         return jsonable_encoder(response)
+
+@app.post("/dict")
+
+def add(word : str):
+    return {"word": word}
+
